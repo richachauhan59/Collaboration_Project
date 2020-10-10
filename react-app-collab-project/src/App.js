63b286e4cce51091f7d54d2ec71d001c0a863cd2
richachauhan59 <richachuahan2019@gmail.com>
2020-10-10 17:39:05 +0530

    Initial Login Commit

--- react-app-collab-project/src/App.js
+++ react-app-collab-project/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './Components/login'
 
 function App() {
   return (
     <div className="App">
-      <h1>App File</h1>
+      <Login/>
     </div>
   );
 }
---
+++ react-app-collab-project/src/Components/login.jsx
@@ -0,0 +1,45 @@
+import React  from 'react'
+
+export default class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            email: "",
+            password:""
+        }
+    }
+
+    handleLogin = () =>{
+        console.log(this.state.email, this.state.password)
+    }
+
+    handleChange = (e) =>{
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render(){
+        const {handleLogin, handleChange} = this
+        const {email, password} = this.state
+        return (
+            <div style={{padding:"2%", border:"2px solid black" , width:"50%", marginLeft:"20%",marginBottom:"9%", borderRadius:"5px", marginTop:"5%"}}>
+                <h1> Login Here! </h1>
+                Email:
+                <input value={email} 
+                onChange={handleChange}
+                name="email"
+                />
+                password
+                <input value={password} 
+                onChange={handleChange}
+                name="password"
+                />
+                <button onClick={handleLogin} >Login</button>
+            </div>
+        )
+    }
+}
+
+
